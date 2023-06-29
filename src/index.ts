21f0e0dc9950383d62534a7fb5a721d95edba9c5
opvel <opvel.hero@gmail.com>
2023-06-30 03:32:39 +0900

    feat: エラーハンドリングとloggingを追加

--- src/index.ts
+++ src/index.ts
@@ -8,66 +8,71 @@ import { QuestionChannelHandler } from "./ChannelHandler/QuestionChannelHandler"
 import OpenAIProcessor from "./OpenAIProcessor/OpenAIProcessor";
 import Pinecone from "./Pinecone/Pinecone";
 import { ChannelType } from "discord.js";
+import logger from "./logger";
 
+// 環境変数を読み込む (開発環境の場合は.env.devを読み込む)
 const envPath = process.env.NODE_ENV === "dev" ? ".env.dev" : ".env";
 
 require("dotenv").config({
   path: envPath,
 });
 
+// すべての環境変数が設定されていない場合はエラーを投げる
+const { CHANNEL_ID_GREETING, CHANNEL_ID_QUESTION, CHANNEL_ID_SEARCH } =
+  process.env;
+if (!CHANNEL_ID_GREETING && !CHANNEL_ID_QUESTION && !CHANNEL_ID_SEARCH) {
+  const errorMessage =
+    "None of the required environment variables are set.(channel id)";
+  logger.error(errorMessage);
+  throw new Error(errorMessage);
+}
+
 async function main() {
-  const openAIProcessor = new OpenAIProcessor();
-  const pinecone = await Pinecone.init();
+  try {
+    const openAIProcessor = new OpenAIProcessor();
+    const pinecone = await Pinecone.init();
 
-  const channelActions = new Map<string, ChannelHandler>();
+    const channelActions = new Map<string, ChannelHandler>();
 
-  if (process.env.CHANNEL_ID_GREETING) {
-    channelActions.set(
-      process.env.CHANNEL_ID_GREETING,
-      new GreetingChannelHandler(openAIProcessor)
-    );
-  }
-
-  if (process.env.CHANNEL_ID_QUESTION) {
-    channelActions.set(
-      process.env.CHANNEL_ID_QUESTION,
-      new QuestionChannelHandler(openAIProcessor)
-    );
-  }
-
-  if (process.env.CHANNEL_ID_SEARCH) {
-    channelActions.set(
-      process.env.CHANNEL_ID_SEARCH,
-      new SearchChannelHandler(openAIProcessor, pinecone)
-    );
-  }
-
-  // すべての環境変数が設定されていない場合はエラーを投げる
-  if (
-    !process.env.CHANNEL_ID_GREETING &&
-    !process.env.CHANNEL_ID_QUESTION &&
-    !process.env.CHANNEL_ID_SEARCH
-  ) {
-    throw new Error(
-      "None of the required environment variables are set.(channel id)"
-    );
-  }
-
-  const bot = new MyBot((message) => {
-    console.log(`Received message: ${message.content}`);
-
-    if (message.channel.type !== ChannelType.GuildText) return;
-    if (message.author.bot) return;
-
-    const channelId = message.channel.id;
-    const handler = channelActions.get(channelId);
-
-    if (handler) {
-      handler.handle(message);
+    if (CHANNEL_ID_GREETING) {
+      channelActions.set(
+        CHANNEL_ID_GREETING,
+        new GreetingChannelHandler(openAIProcessor)
+      );
     }
-  });
 
-  bot.start();
+    if (CHANNEL_ID_QUESTION) {
+      channelActions.set(
+        CHANNEL_ID_QUESTION,
+        new QuestionChannelHandler(openAIProcessor)
+      );
+    }
+
+    if (CHANNEL_ID_SEARCH) {
+      channelActions.set(
+        CHANNEL_ID_SEARCH,
+        new SearchChannelHandler(openAIProcessor, pinecone)
+      );
+    }
+
+    const bot = new MyBot((message) => {
+      logger.info(`Received message: ${message.content}`);
+
+      if (message.channel.type !== ChannelType.GuildText) return;
+      if (message.author.bot) return;
+
+      const channelId = message.channel.id;
+      const handler = channelActions.get(channelId);
+
+      if (handler) {
+        handler.handle(message);
+      }
+    });
+
+    await bot.start();
+  } catch (error) {
+    logger.error(error);
+  }
 }
 
 main();
