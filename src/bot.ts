a9c7612bd3bd6d1ee5c62730880dc181c8d93c3f
opvel <opvel.hero@gmail.com>
2023-06-30 03:13:48 +0900

    refactor: Bot classの構造を改善

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,20 @@
 import { Client, Events, GatewayIntentBits, Message } from "discord.js";
+import logger from "./logger";
 
+// Discord Botのクラス
 class MyBot {
   private client: Client;
   private messageHandler: (message: Message) => void;
 
+  /**
+   * MyBotのコンストラクタ。
+   * @param messageHandler メッセージを処理するためのコールバック関数。
+   */
   constructor(messageHandler: (message: Message) => void) {
+    // メッセージハンドラを初期化
+    this.messageHandler = messageHandler;
+
+    // Discordクライアントを作成
     this.client = new Client({
       intents: [
         GatewayIntentBits.Guilds,
@@ -13,19 +23,34 @@ class MyBot {
       ],
     });
 
+    // クライアントが準備完了したときのイベントハンドラ
     this.client.once(Events.ClientReady, (c) => {
-      console.log(`Ready! Logged in as ${c.user.tag}`);
+      logger.info(`Ready! Logged in as ${c.user.tag}`);
     });
 
-    this.client.on("messageCreate", (message: Message) => {
+    // 新しいメッセージが作成されたときに呼び出されるイベントハンドラ
+    this.client.on("messageCreate", (message) => {
       this.messageHandler(message);
     });
-
-    this.messageHandler = messageHandler;
   }
 
-  public start() {
-    this.client.login(process.env.DISCORD_TOKEN);
+  /**
+   * Botを開始します。環境変数からトークンを読み取り、Discordにログインします。
+   */
+  public async start() {
+    const token = process.env.DISCORD_TOKEN;
+    if (!token) {
+      logger.error("DISCORD_TOKEN is not set in environment variables.");
+      return;
+    }
+
+    try {
+      // Discordにログイン
+      await this.client.login(token);
+      logger.info("Bot has started successfully.");
+    } catch (error) {
+      logger.error("Error starting the bot:", error);
+    }
   }
 }
 
