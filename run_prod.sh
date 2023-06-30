5dfbf3e0b6c5bad5d8435a6eedab3ad1737b9a50
opvel <opvel.hero@gmail.com>
2023-07-01 02:30:17 +0900

    feat: logging機能を追加

---
+++ run_prod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# パス、ユーザー名、グループ名を動的に取得
+LOG_DIR=$(realpath ./logs) # ログディレクトリの絶対パスを取得
+USERNAME=$(id -un)
+GROUP=$(id -gn)
+
+# logrotateの設定ファイルを生成
+cat > /etc/logrotate.d/mybot <<EOL
+${LOG_DIR}/mybot.log {
+    daily
+    rotate 3
+    compress
+    missingok
+    notifempty
+    create 0640 $USERNAME $GROUP
+    postrotate
+        kill -HUP \`cat ${LOG_DIR}/mybot.pid\`
+    endscript
+}
+EOL
+
+# その他のプロダクション用のコマンドを実行
+tsc --build && node ./build/index.js
--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,26 @@
 // logger.ts
 import pino from "pino";
+import fs from "fs";
+import path from "path";
+
+// 絶対パスを使用してlogsディレクトリを指定
+const logsDir = path.resolve(__dirname, "..", "logs");
+
+// logsディレクトリが存在しない場合は作成
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir, { recursive: true });
+}
+
+// プロセスIDをファイルに書き込む
+const pidFilePath = path.join(logsDir, "mybot.pid");
+fs.writeFileSync(pidFilePath, process.pid.toString());
 
 // ロガーを初期化
-const logger = pino();
+const logFilePath = path.join(logsDir, "mybot.log");
+const dest = pino.destination(logFilePath);
+const logger = pino(dest);
+
+// SIGHUPシグナルを受信したときにログファイルを再オープン
+process.on("SIGHUP", () => dest.reopen());
 
 export default logger;
