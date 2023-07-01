fb51ab561f9f4249e735a8f31e17fd75922b72ee
opvel <opvel.hero@gmail.com>
2023-07-02 03:50:38 +0900

    feat: 面倒なのでlog出力をerrorのみにしてstdoutに出すようにする。

--- src/logger.ts
+++ src/logger.ts
@@ -1,39 +1,27 @@
 // logger.ts
 import pino from "pino";
-import fs from "fs";
-import path from "path";
 
 // 開発環境かどうかをチェック
 const isDevEnv = process.env.NODE_ENV === "dev";
 
+const baseLoggerConfig: pino.LoggerOptions = {
+  // ログレベルの設定
+  level: isDevEnv ? "debug" : "error",
+};
+
 let logger: pino.Logger;
 
 if (isDevEnv) {
+  // 開発環境の場合は pino-pretty を使って見やすく出力する
   logger = pino({
+    ...baseLoggerConfig,
     transport: {
       target: "pino-pretty",
     },
   });
 } else {
-  // 絶対パスを使用してlogsディレクトリを指定
-  const logsDir = path.resolve(__dirname, "..", "logs");
-
-  // logsディレクトリが存在しない場合は作成
-  if (!fs.existsSync(logsDir)) {
-    fs.mkdirSync(logsDir, { recursive: true });
-  }
-
-  // プロセスIDをファイルに書き込む
-  const pidFilePath = path.join(logsDir, "mybot.pid");
-  fs.writeFileSync(pidFilePath, process.pid.toString());
-
-  // ロガーを初期化
-  const logFilePath = path.join(logsDir, "mybot.log");
-  const dest = pino.destination(logFilePath);
-  logger = pino(dest);
-
-  // SIGHUPシグナルを受信したときにログファイルを再オープン
-  process.on("SIGHUP", () => dest.reopen());
+  // 本番環境の場合はデフォルトの設定を使用する
+  logger = pino(baseLoggerConfig);
 }
 
 export default logger;
