11d7c70c5b677a86bfe170738efaeccf91df8e54
opvel <opvel.hero@gmail.com>
2023-07-02 19:39:30 +0900

    refactor: 移動

--- src/MessageHandler/configLoader.ts
+++ src/MessageHandler/configLoader.ts
@@ -1,11 +1,5 @@
 import logger from "../logger";
 
-// 環境変数を読み込む (開発環境の場合は.env.devを読み込む)
-const envPath = process.env.NODE_ENV === "dev" ? ".env.dev" : ".env";
-require("dotenv").config({
-  path: envPath,
-});
-
 const { CHANNEL_ID_GREETING, CHANNEL_ID_QUESTION, CHANNEL_ID_SEARCH } =
   process.env;
 // すべての環境変数が設定されていない場合はエラーを投げる
